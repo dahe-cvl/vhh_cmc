827097acbd35e07c93a293133871a9040584d5c0
dhelm <dhelm@cvl.tuwien.ac.at>
2020-06-09 09:56:34 +0200

    - change version in setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ print("all dependencies installed!")
 # install sbd package
 setup(
      name='vhh_cmc',
-     version='0.1.0',
+     version='1.0.0',
      author="Daniel Helm",
      author_email="daniel.helm@tuwien.ac.at",
      description="Camera Movements Classification Package",

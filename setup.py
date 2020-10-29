e46794bf9f3d6e67ee2c8e526798536fe1f5a5d4
User <dhelm@zapp.cvl.tuwien.ac.at>
2020-10-29 15:10:35 +0100

    release v1.2.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("requirements.txt", "r") as f:
 # install vhh_sbd package
 setup(
      name='vhh_cmc',
-     version='1.0.0',
+     version='1.2.0',
      author="Daniel Helm",
      author_email="daniel.helm@tuwien.ac.at",
      description="Camera Movements Classification Package",

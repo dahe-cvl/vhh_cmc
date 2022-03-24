1c223321d0c09f848d046da000194b9af3dfe1fd
User <dhelm@zapp.cvl.tuwien.ac.at>
2022-03-24 14:54:13 +0100

    initial commit for release v1.3.0

--- ApiSphinxDocumentation/source/conf.py
+++ ApiSphinxDocumentation/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Daniel Helm'
 author = 'Daniel Helm'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.3.0'
 
 
 # -- General configuration ---------------------------------------------------
